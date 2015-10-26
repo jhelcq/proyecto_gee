60fd670e7d43c0af7411753d606b8e212ad03a60
Jhonny Colmena <jhoelicq@hotmail.com>
2015-10-26 10:40:56 +0100

    mostrando el mapa de cobertura global solo en el area de Bolivia

--- cut_imagen_bolivia_cobertura.js
+++ cut_imagen_bolivia_cobertura.js
@@ -1,12 +1,15 @@
-var vectorBolivia = ee.FeatureCollection('ft:1RxLMu2UiIzvtVd-WqmnKRp7Vjob0JyKEiG2nxTeL'); //cargamos el kml de Bolivia
-var imagenCoberturaGlobal= ee.Image('ESA/GLOBCOVER_L4_200901_200912_V2_3'); //cargamos la imagen de mapa de cobertura
+var rasterBolivia = ee.FeatureCollection('ft:1RxLMu2UiIzvtVd-WqmnKRp7Vjob0JyKEiG2nxTeL'); //cargamos el kml de Bolivia
+var imgCoverGlobal= ee.Image('ESA/GLOBCOVER_L4_200901_200912_V2_3'); //cargamos la imagen de mapa de cobertura global
+
+var imgCoverBolivia= imgCoverGlobal.clip(rasterBolivia); //recortamos la imagen de cobertura global en el area de Bolivia
 
 // exportamos la imagen de cobertura en el area de Bolivia
-Export.image(imagenCoberturaGlobal, 'export_cobertura_bolivia', {
+Export.image(imgCoverBolivia, 'export_cobertura_bolivia', {
   	scale: 7,
-  	region: vectorBolivia.geometry().getInfo(),
+  	region: rasterBolivia.geometry().getInfo(),
 	maxPixels: 50000000000 //cambiamos el maximo de pixeles
 });
 
-Map.centerObject(vectorBolivia, 6);  //centreamos el mapa en base al vector de Bolivia con un zoom de lejano
-Map.addLayer(vectorBolivia); //mostramos el vector de Bolivia en el mapa
+Map.centerObject(rasterBolivia, 6);  //centramos el mapa en base al vector de Bolivia con un zoom lejano
+Map.addLayer(rasterBolivia); //mostramos el vector de Bolivia en el mapa
+Map.addLayer(imgCoverBolivia); //mostramos la imagen de cobertura de Bolivia
