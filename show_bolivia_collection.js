1971938074ab1a5efdd86be84ee4cf4a66113773
Mike Fox <mikefoxtrotboa@gmail.com>
2015-10-27 16:07:10 -0430

    Update show_bolivia_collection.js

--- show_bolivia_collection.js
+++ show_bolivia_collection.js
@@ -4,5 +4,5 @@ var usgs_toa= ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
 	.filterDate('2015-01-01', '2015-10-20');
 
 Map.centerObject(vectorSantaCruz, 6);  //centramos el mapa en base al vector de Bolivia con un zoom lejano
-Mapp.addLayer(usgs_toa);
+Mapp.addLayer(usgs_toa); //adicionando capa
 Mapp.addLayer(vectorSantaCruz);
