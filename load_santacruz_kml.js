32fde55a7a02e8054f629b3f282448e62c5eeabf
Jhonny Colmena <jhoelicq@hotmail.com>
2015-10-30 16:28:45 +0100

    centrado el kml en el mapa

--- load_santacruz_kml.js
+++ load_santacruz_kml.js
@@ -5,4 +5,5 @@ var vectorSantaCruz = ee.FeatureCollection('ft:1eT0En9df3oNiGdA6-93FnYtpXnSqZ6nw
 2do par: se envia las opciones de visualizacion
 3er par: creo que solo es un comentario como string
 */
+Map.centerObject(vectorSantaCruz, 9);  //centreamos el mapa en base a la primera imagen
 Map.addLayer(vectorSantaCruz);  //adiciona el vector de punto en el mapa
