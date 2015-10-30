e777e741902ab76294740c6f8e8bc15a09e6466f
Jhonny Colmena <jhoelicq@hotmail.com>
2015-10-30 18:10:58 +0100

    adicionando script de obtencion de tabla de cobertura y meses de imagenes de landsat

--- show_bolivia_collection.js
+++ show_bolivia_collection.js
@@ -1,8 +1,24 @@
-var vectorSantaCruz = ee.FeatureCollection('ft:1RxLMu2UiIzvtVd-WqmnKRp7Vjob0JyKEiG2nxTeL'); //cargamos el kml de santa cruz
-var usgs_toa= ee.ImageCollection('LANDSAT/LC8_L1T_TOA')
-	.filterBounds(vectorSantaCruz)
-	.filterDate('2015-01-01', '2015-10-20');
+var rasterBolivia = ee.FeatureCollection('ft:1RxLMu2UiIzvtVd-WqmnKRp7Vjob0JyKEiG2nxTeL'); //cargamos el kml de bolivia
+var landsatUsgsToa= ee.ImageCollection('LANDSAT/LC8_L1T_TOA') //las imagenes landsat8 de USGS
+	.filterBounds(rasterBolivia) //filtramos la col. de imagenes solo para Bolivia
+	.filterDate('2015-01-01', '2015-10-20') //filtramos imagenes solo del año 2015
+	.filterMetadata('CLOUD_COVER', 'less_than', 10.0); //solo obtenemos las imagenes con un maximo de nubosidad de 10%
 
-Map.centerObject(vectorSantaCruz, 6);  //centramos el mapa en base al vector de Bolivia con un zoom lejano
-Mapp.addLayer(usgs_toa);
-Mapp.addLayer(vectorSantaCruz);
+var feaColMetaImagenes = landsatUsgsToa.map(function(img) {  //iteramos sobre cada una de las imagenes de una coleccion de imagenes
+	var coberturaNube = ee.Image(img).get('CLOUD_COVER');  //obtenemos el metadato de nubosidad de la imagen
+	var fechaAdquisicion = ee.Image(img).get('DATE_ACQUIRED'); //fecha de adquisicion de la imagen
+	return ee.Feature(null, {'score': coberturaNube, 'date': fechaAdquisicion })
+});
+
+//exportamos como tabla la feature collection de metadatos de la imagen
+Export.table(feaColMetaImagenes, 'export_metadatos_imgbolivia', {
+  	gmeAttributionName: 'score',
+	driveFolder: 'cobuso_fao',
+	driveFileNamePrefix: 'cover_metadatos_imgbolivia',
+  	fileFormat: 'CSV' //formato de salida
+});
+
+// print(Chart.feature.byFeature(feaColMetaImageness);  //mostramos los metadatos de las imagenes como un grafico estadistico
+Map.centerObject(rasterBolivia, 6);
+Map.addLayer(landsatUsgsToa);
+Map.addLayer(rasterBolivia);
