8bcae248bea3717faa0e4f9bb9925a0a1a2b6d84
jhelcq <jhoelicq@hotmail.com>
2015-11-02 10:50:13 -0400

    refactorizando la carga de vector en formato kml

--- carga_vector.js
+++ carga_vector.js
@@ -1,9 +1,25 @@
-//carga un archivo kml(adicionado a google drive) que tiene un determinado ID
-var vectorSantaCruz = ee.FeatureCollection('ft:1eT0En9df3oNiGdA6-93FnYtpXnSqZ6nwtp6nCn3s'); //cargamos el kml de santa cruz
-
-/*1do par: una imagen, una collecion de imagenes o un vector(feautre collection)
-2do par: se envia las opciones de visualizacion
-3er par: creo que solo es un comentario como string
+/*
+	Autor: Jhonny Colmena
+	Email: jhoelicq@hotmail.com
+	Fecha: 2015-10-20
+	Descripcion: Script de carga de un vector en formato kml
 */
-Map.centerObject(vectorSantaCruz, 9);  //centreamos el mapa en base a la primera imagen
-Map.addLayer(vectorSantaCruz);  //adiciona el vector de punto en el mapa
+
+function cargarVector(idVector){
+	//carga un archivo kml(adicionado a google drive) que tiene un determinado ID
+	return vector = ee.FeatureCollection('ft:'+ idVector); //cargamos el kml de Bolivia
+}
+
+function mostrarRecurso(recurso){
+	Map.addLayer(recurso);  //adiciona el vector, la imagen o la coleccion de imagenes al mapa
+	
+	/*1do par: una imagen, una collecion de imagenes o un vector(feautre collection)
+	2do par: se envia las opciones de visualizacion
+	3er par: creo que solo es un comentario como string*/
+	Map.centerObject(recurso, 9);  //centreamos el mapa en base a la imagen, al vector.
+}
+
+var vectorBolivia= cargarVector('1RxLMu2UiIzvtVd-WqmnKRp7Vjob0JyKEiG2nxTeL'); //cargamos el vector de Bolivia
+mostrarRecurso(vectorBolivia); //mostramos el vector de Bolivia
+
+
